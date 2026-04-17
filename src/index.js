e010a52a7fe0790cc8a0955a3eecf97e10053b52
Andreu Monforte <andreump5@gmail.com>
2026-04-17 19:47:48 +0200

    feat Entry: Make Iteration 0 & 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    getFormattedAmount() {
+        return `${this.amount} €`
+    }
+}
 
 // Income
-class Income {}
+class Income {
+
+}
 
 // Expense
 class Expense {}
