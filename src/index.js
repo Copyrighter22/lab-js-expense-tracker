70b4e322a290bb8400f94309cff6cdc00c3bca62
Andreu Monforte <andreump5@gmail.com>
2026-04-17 20:12:27 +0200

    feat Income: Make Iteration 2

--- src/index.js
+++ src/index.js
@@ -12,8 +12,12 @@ class Entry {
 }
 
 // Income
-class Income {
-
+class Income extends Entry {
+    constructor(date, amount, description){
+        super(date, amount, description)
+        this.type = "income"
+        
+    }
 }
 
 // Expense
